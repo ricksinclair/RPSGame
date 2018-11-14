3ddda5b152e4d2ff2dc9eccf53cb1e4f27d035a1
Ulric Todman <rick.sinclair.nyc@gmail.com>
2018-11-14 10:23:37 -0500

    changed svg location for faster loading

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,66 @@
 /* Psuedo code 
   
-  
+  i plan on using switch statements to manage player enrollment ability using child counts
 
-  Step 1. Define variables
+  i need a little more time I know i needed MVP but I'm trying.
+
+  Step 1. Define game variables
           Variables, properties, and methods needed:
                                                     -playerCount
-                                                    -player1(values)
+                                                    -playerPrototype
                                                       -1 player1.choice 
                                                       -1 player1.name 
                                                       -1 player1.location
                                                       -1 player1.wins ( i want these in local storage)
                                                       -1 player1.losses  ( i want these in local storage)
-                                                     -player2 (values)
-                                                      -player2.choice
-                                                      -player2.name
-                                                      -player2.location
-                                                      -player2.wins  ( i want these in local storage)
-                                                      -player2.losses  ( i want these in local storage)
+                                                    
+                                                      i want these in local storage)
                                                      -gameState(methods)
                                                       -playerRegistration
                                                       -chooseHands
                                                       -results
                                                       -newGame
                                                       -newPlayer(1 or 2)
+*/
 
+//initialize firebase
+var config = {
+  apiKey: "AIzaSyCT2jUeOfrDu4i_rwQFIfKw1kMcEWc1Zao",
+  authDomain: "rpsgame-9d00b.firebaseapp.com",
+  databaseURL: "https://rpsgame-9d00b.firebaseio.com",
+  projectId: "rpsgame-9d00b",
+  storageBucket: "",
+  messagingSenderId: "326094420080"
+};
+
+firebase.initializeApp(config);
+
+var gameMechanics = {
+  localID: 0,
+  playerCount: 0,
+  player: class {
+    constructor(name, age, country) {
+      this.name = name;
+      this.age = age;
+      this.country = country;
+      this.wins = 0;
+      this.losses = 0;
+      this.choice;
+      this.playerOne = false;
+      this.PlayerTwo = false;
+    }
+  },
+
+  choiceLogic: () => {},
+
+  enrollmentLogic: () => {
+    $("#submitPlayer").on("submit", event => {
+      event.preventDefault();
+    });
+  }
+};
+
+/*
   Step 2. Define win/loss logic using nested switch/case statements(
                                       they'll nest  like:
                                       
@@ -42,32 +79,9 @@
                                       }
                                       
                                       (since two parties choices need to be considered) 
+
+
+
+
+
   */
-
-///////////////////////////////////  ,/
-//////////Declare variables/////////
-///////////////////////////////////
-
-//This will display the
-
-function Player(name, age, location) {
-  this.name = name;
-  this.age = age;
-  this.location = location;
-}
-
-var playerOne = new Player();
-var playerTwo = new Player();
-// Initialize Firebase
-var config = {
-  apiKey: "AIzaSyCT2jUeOfrDu4i_rwQFIfKw1kMcEWc1Zao",
-  authDomain: "rpsgame-9d00b.firebaseapp.com",
-  databaseURL: "https://rpsgame-9d00b.firebaseio.com",
-  projectId: "rpsgame-9d00b",
-  storageBucket: "",
-  messagingSenderId: "326094420080"
-};
-
-firebase.initializeApp(config);
-
-$("#svg-container").hide();
